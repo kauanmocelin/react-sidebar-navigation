67cf746e150af701cb1c4c290f3e44c31d7d3e1b
kauanmocelin <kauanmocelin@gmail.com>
2020-11-14 16:22:10 -0300

    Adição de componente de estilo global com styled components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home'
 import Reports from './pages/Reports'
 import Products from './pages/Products'
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/reports' component={Reports} />
-        <Route path='/products' component={Products} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter BrowserRouter >
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </BrowserRouter >
+    </>
   );
 }
 
